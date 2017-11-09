5ba69d8cbed3fb7a49d842cb88e427c7251c62a7
Junior <junior_turatto@hotmail.com>
2017-11-09 09:25:03 -0200

    Cadastro Empresa

--- AluguelDeCarros/AluguelDeCarros/Form1.Designer.cs
+++ AluguelDeCarros/AluguelDeCarros/Form1.Designer.cs
@@ -28,20 +28,334 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtEmpresa = new System.Windows.Forms.Label();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.lblRua = new System.Windows.Forms.Label();
+            this.txtCid = new System.Windows.Forms.TextBox();
+            this.lblCid = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEst = new System.Windows.Forms.TextBox();
+            this.lblEst = new System.Windows.Forms.Label();
+            this.mskRG = new System.Windows.Forms.MaskedTextBox();
+            this.lblRG = new System.Windows.Forms.Label();
+            this.mskCPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblCel = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.mskCel = new System.Windows.Forms.MaskedTextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.mskTel = new System.Windows.Forms.MaskedTextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(547, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 59);
+            this.button1.TabIndex = 120;
+            this.button1.Text = "Voltar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 170);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 119;
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.AutoSize = true;
+            this.txtEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpresa.ForeColor = System.Drawing.Color.Crimson;
+            this.txtEmpresa.Location = new System.Drawing.Point(107, 147);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(156, 20);
+            this.txtEmpresa.TabIndex = 118;
+            this.txtEmpresa.Text = "Nome da Empresa";
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(492, 170);
+            this.txtRua.MaxLength = 100;
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(144, 20);
+            this.txtRua.TabIndex = 115;
+            // 
+            // lblRua
+            // 
+            this.lblRua.AutoSize = true;
+            this.lblRua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRua.ForeColor = System.Drawing.Color.Crimson;
+            this.lblRua.Location = new System.Drawing.Point(488, 147);
+            this.lblRua.Name = "lblRua";
+            this.lblRua.Size = new System.Drawing.Size(42, 20);
+            this.lblRua.TabIndex = 117;
+            this.lblRua.Text = "Rua";
+            // 
+            // txtCid
+            // 
+            this.txtCid.Location = new System.Drawing.Point(342, 170);
+            this.txtCid.MaxLength = 100;
+            this.txtCid.Name = "txtCid";
+            this.txtCid.Size = new System.Drawing.Size(132, 20);
+            this.txtCid.TabIndex = 114;
+            // 
+            // lblCid
+            // 
+            this.lblCid.AutoSize = true;
+            this.lblCid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCid.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCid.Location = new System.Drawing.Point(338, 147);
+            this.lblCid.Name = "lblCid";
+            this.lblCid.Size = new System.Drawing.Size(65, 20);
+            this.lblCid.TabIndex = 116;
+            this.lblCid.Text = "Cidade";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(111, 115);
+            this.txtSenha.MaxLength = 100;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(292, 20);
+            this.txtSenha.TabIndex = 100;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.Crimson;
+            this.lblSenha.Location = new System.Drawing.Point(107, 92);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(61, 20);
+            this.lblSenha.TabIndex = 113;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(111, 65);
+            this.txtEmail.MaxLength = 100;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(292, 20);
+            this.txtEmail.TabIndex = 99;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.Crimson;
+            this.lblEmail.Location = new System.Drawing.Point(107, 42);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 112;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEst
+            // 
+            this.txtEst.Location = new System.Drawing.Point(418, 117);
+            this.txtEst.MaxLength = 100;
+            this.txtEst.Name = "txtEst";
+            this.txtEst.Size = new System.Drawing.Size(218, 20);
+            this.txtEst.TabIndex = 106;
+            // 
+            // lblEst
+            // 
+            this.lblEst.AutoSize = true;
+            this.lblEst.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEst.ForeColor = System.Drawing.Color.Crimson;
+            this.lblEst.Location = new System.Drawing.Point(414, 94);
+            this.lblEst.Name = "lblEst";
+            this.lblEst.Size = new System.Drawing.Size(66, 20);
+            this.lblEst.TabIndex = 111;
+            this.lblEst.Text = "Estado";
+            // 
+            // mskRG
+            // 
+            this.mskRG.Location = new System.Drawing.Point(536, 19);
+            this.mskRG.Mask = "99\\.999\\.999\\-9";
+            this.mskRG.Name = "mskRG";
+            this.mskRG.Size = new System.Drawing.Size(100, 20);
+            this.mskRG.TabIndex = 102;
+            this.mskRG.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lblRG
+            // 
+            this.lblRG.AutoSize = true;
+            this.lblRG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRG.ForeColor = System.Drawing.Color.Crimson;
+            this.lblRG.Location = new System.Drawing.Point(532, -4);
+            this.lblRG.Name = "lblRG";
+            this.lblRG.Size = new System.Drawing.Size(36, 20);
+            this.lblRG.TabIndex = 110;
+            this.lblRG.Text = "RG";
+            // 
+            // mskCPF
+            // 
+            this.mskCPF.Location = new System.Drawing.Point(418, 19);
+            this.mskCPF.Mask = "999\\.999\\.999\\-99";
+            this.mskCPF.Name = "mskCPF";
+            this.mskCPF.Size = new System.Drawing.Size(100, 20);
+            this.mskCPF.TabIndex = 101;
+            this.mskCPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // lblCel
+            // 
+            this.lblCel.AutoSize = true;
+            this.lblCel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCel.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCel.Location = new System.Drawing.Point(532, 42);
+            this.lblCel.Name = "lblCel";
+            this.lblCel.Size = new System.Drawing.Size(65, 20);
+            this.lblCel.TabIndex = 108;
+            this.lblCel.Text = "Celular";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTel.ForeColor = System.Drawing.Color.Crimson;
+            this.lblTel.Location = new System.Drawing.Point(414, 42);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(79, 20);
+            this.lblTel.TabIndex = 107;
+            this.lblTel.Text = "Telefone";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCPF.Location = new System.Drawing.Point(414, -4);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(43, 20);
+            this.lblCPF.TabIndex = 103;
+            this.lblCPF.Text = "CPF";
+            // 
+            // mskCel
+            // 
+            this.mskCel.Location = new System.Drawing.Point(536, 65);
+            this.mskCel.Mask = "\\(99\\) 99999 \\- 9999";
+            this.mskCel.Name = "mskCel";
+            this.mskCel.Size = new System.Drawing.Size(100, 20);
+            this.mskCel.TabIndex = 105;
+            this.mskCel.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(111, 19);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(292, 20);
+            this.txtNome.TabIndex = 98;
+            // 
+            // mskTel
+            // 
+            this.mskTel.Location = new System.Drawing.Point(418, 65);
+            this.mskTel.Mask = "\\(99\\) 9999 \\- 9999";
+            this.mskTel.Name = "mskTel";
+            this.mskTel.Size = new System.Drawing.Size(100, 20);
+            this.mskTel.TabIndex = 104;
+            this.mskTel.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
+            this.btnSalvar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalvar.Location = new System.Drawing.Point(107, 196);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(159, 59);
+            this.btnSalvar.TabIndex = 109;
+            this.btnSalvar.Text = "Cadastrar";
+            this.btnSalvar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.Crimson;
+            this.lblNome.Location = new System.Drawing.Point(107, -4);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 97;
+            this.lblNome.Text = "Nome";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(742, 250);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.lblRua);
+            this.Controls.Add(this.txtCid);
+            this.Controls.Add(this.lblCid);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEst);
+            this.Controls.Add(this.lblEst);
+            this.Controls.Add(this.mskRG);
+            this.Controls.Add(this.lblRG);
+            this.Controls.Add(this.mskCPF);
+            this.Controls.Add(this.lblCel);
+            this.Controls.Add(this.lblTel);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.mskCel);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.mskTel);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblNome);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtEmpresa;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.Label lblRua;
+        private System.Windows.Forms.TextBox txtCid;
+        private System.Windows.Forms.Label lblCid;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEst;
+        private System.Windows.Forms.Label lblEst;
+        private System.Windows.Forms.MaskedTextBox mskRG;
+        private System.Windows.Forms.Label lblRG;
+        private System.Windows.Forms.MaskedTextBox mskCPF;
+        private System.Windows.Forms.Label lblCel;
+        private System.Windows.Forms.Label lblTel;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.MaskedTextBox mskCel;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.MaskedTextBox mskTel;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label lblNome;
     }
 }
 
