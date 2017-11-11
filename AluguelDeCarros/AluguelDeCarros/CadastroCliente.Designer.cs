e78319ad4dd65b35888381ce1c83019d65ed501b
Juniorxpxd <junior_turatto@hotmail.com>
2017-11-11 16:28:03 -0200

    Cadastro do Cliente

--- AluguelDeCarros/AluguelDeCarros/CadastroCliente.Designer.cs
+++ AluguelDeCarros/AluguelDeCarros/CadastroCliente.Designer.cs
@@ -30,8 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroCliente));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txtEmpresa = new System.Windows.Forms.Label();
             this.txtRua = new System.Windows.Forms.TextBox();
             this.lblRua = new System.Windows.Forms.Label();
             this.txtCid = new System.Windows.Forms.TextBox();
@@ -53,6 +51,8 @@
             this.mskTel = new System.Windows.Forms.MaskedTextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.lblNome = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -69,60 +69,43 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(3, 183);
-            this.textBox1.MaxLength = 100;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 20);
-            this.textBox1.TabIndex = 143;
-            // 
-            // txtEmpresa
-            // 
-            this.txtEmpresa.AutoSize = true;
-            this.txtEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEmpresa.ForeColor = System.Drawing.Color.Crimson;
-            this.txtEmpresa.Location = new System.Drawing.Point(-1, 160);
-            this.txtEmpresa.Name = "txtEmpresa";
-            this.txtEmpresa.Size = new System.Drawing.Size(156, 20);
-            this.txtEmpresa.TabIndex = 142;
-            this.txtEmpresa.Text = "Nome da Empresa";
-            // 
             // txtRua
             // 
-            this.txtRua.Location = new System.Drawing.Point(384, 183);
+            this.txtRua.Location = new System.Drawing.Point(250, 183);
             this.txtRua.MaxLength = 100;
             this.txtRua.Name = "txtRua";
-            this.txtRua.Size = new System.Drawing.Size(144, 20);
+            this.txtRua.Size = new System.Drawing.Size(160, 20);
             this.txtRua.TabIndex = 139;
             // 
             // lblRua
             // 
             this.lblRua.AutoSize = true;
-            this.lblRua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRua.ForeColor = System.Drawing.Color.Crimson;
-            this.lblRua.Location = new System.Drawing.Point(380, 160);
+            this.lblRua.BackColor = System.Drawing.Color.Transparent;
+            this.lblRua.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRua.ForeColor = System.Drawing.Color.White;
+            this.lblRua.Location = new System.Drawing.Point(246, 160);
             this.lblRua.Name = "lblRua";
-            this.lblRua.Size = new System.Drawing.Size(42, 20);
+            this.lblRua.Size = new System.Drawing.Size(39, 22);
             this.lblRua.TabIndex = 141;
             this.lblRua.Text = "Rua";
             // 
             // txtCid
             // 
-            this.txtCid.Location = new System.Drawing.Point(234, 183);
+            this.txtCid.Location = new System.Drawing.Point(3, 183);
             this.txtCid.MaxLength = 100;
             this.txtCid.Name = "txtCid";
-            this.txtCid.Size = new System.Drawing.Size(132, 20);
+            this.txtCid.Size = new System.Drawing.Size(231, 20);
             this.txtCid.TabIndex = 138;
             // 
             // lblCid
             // 
             this.lblCid.AutoSize = true;
-            this.lblCid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCid.ForeColor = System.Drawing.Color.Crimson;
-            this.lblCid.Location = new System.Drawing.Point(230, 160);
+            this.lblCid.BackColor = System.Drawing.Color.Transparent;
+            this.lblCid.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCid.ForeColor = System.Drawing.Color.White;
+            this.lblCid.Location = new System.Drawing.Point(-1, 160);
             this.lblCid.Name = "lblCid";
-            this.lblCid.Size = new System.Drawing.Size(65, 20);
+            this.lblCid.Size = new System.Drawing.Size(63, 22);
             this.lblCid.TabIndex = 140;
             this.lblCid.Text = "Cidade";
             // 
@@ -137,11 +120,12 @@
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSenha.ForeColor = System.Drawing.Color.Crimson;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.White;
             this.lblSenha.Location = new System.Drawing.Point(-1, 105);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(61, 20);
+            this.lblSenha.Size = new System.Drawing.Size(56, 22);
             this.lblSenha.TabIndex = 137;
             this.lblSenha.Text = "Senha";
             // 
@@ -156,11 +140,12 @@
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.ForeColor = System.Drawing.Color.Crimson;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
             this.lblEmail.Location = new System.Drawing.Point(-1, 55);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.Size = new System.Drawing.Size(53, 22);
             this.lblEmail.TabIndex = 136;
             this.lblEmail.Text = "Email";
             // 
@@ -175,11 +160,12 @@
             // lblEst
             // 
             this.lblEst.AutoSize = true;
-            this.lblEst.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEst.ForeColor = System.Drawing.Color.Crimson;
+            this.lblEst.BackColor = System.Drawing.Color.Transparent;
+            this.lblEst.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEst.ForeColor = System.Drawing.Color.White;
             this.lblEst.Location = new System.Drawing.Point(306, 107);
             this.lblEst.Name = "lblEst";
-            this.lblEst.Size = new System.Drawing.Size(66, 20);
+            this.lblEst.Size = new System.Drawing.Size(60, 22);
             this.lblEst.TabIndex = 135;
             this.lblEst.Text = "Estado";
             // 
@@ -195,11 +181,12 @@
             // lblRG
             // 
             this.lblRG.AutoSize = true;
-            this.lblRG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRG.ForeColor = System.Drawing.Color.Crimson;
+            this.lblRG.BackColor = System.Drawing.Color.Transparent;
+            this.lblRG.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRG.ForeColor = System.Drawing.Color.White;
             this.lblRG.Location = new System.Drawing.Point(424, 9);
             this.lblRG.Name = "lblRG";
-            this.lblRG.Size = new System.Drawing.Size(36, 20);
+            this.lblRG.Size = new System.Drawing.Size(34, 22);
             this.lblRG.TabIndex = 134;
             this.lblRG.Text = "RG";
             // 
@@ -215,33 +202,36 @@
             // lblCel
             // 
             this.lblCel.AutoSize = true;
-            this.lblCel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCel.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCel.BackColor = System.Drawing.Color.Transparent;
+            this.lblCel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCel.ForeColor = System.Drawing.Color.White;
             this.lblCel.Location = new System.Drawing.Point(424, 55);
             this.lblCel.Name = "lblCel";
-            this.lblCel.Size = new System.Drawing.Size(65, 20);
+            this.lblCel.Size = new System.Drawing.Size(65, 22);
             this.lblCel.TabIndex = 132;
             this.lblCel.Text = "Celular";
             // 
             // lblTel
             // 
             this.lblTel.AutoSize = true;
-            this.lblTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTel.ForeColor = System.Drawing.Color.Crimson;
+            this.lblTel.BackColor = System.Drawing.Color.Transparent;
+            this.lblTel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTel.ForeColor = System.Drawing.Color.White;
             this.lblTel.Location = new System.Drawing.Point(306, 55);
             this.lblTel.Name = "lblTel";
-            this.lblTel.Size = new System.Drawing.Size(79, 20);
+            this.lblTel.Size = new System.Drawing.Size(74, 22);
             this.lblTel.TabIndex = 131;
             this.lblTel.Text = "Telefone";
             // 
             // lblCPF
             // 
             this.lblCPF.AutoSize = true;
-            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCPF.ForeColor = System.Drawing.Color.Crimson;
+            this.lblCPF.BackColor = System.Drawing.Color.Transparent;
+            this.lblCPF.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.ForeColor = System.Drawing.Color.White;
             this.lblCPF.Location = new System.Drawing.Point(306, 9);
             this.lblCPF.Name = "lblCPF";
-            this.lblCPF.Size = new System.Drawing.Size(43, 20);
+            this.lblCPF.Size = new System.Drawing.Size(41, 22);
             this.lblCPF.TabIndex = 127;
             this.lblCPF.Text = "CPF";
             // 
@@ -276,7 +266,7 @@
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
             this.btnSalvar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSalvar.Location = new System.Drawing.Point(-1, 209);
+            this.btnSalvar.Location = new System.Drawing.Point(3, 209);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(159, 59);
             this.btnSalvar.TabIndex = 133;
@@ -288,22 +278,45 @@
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.ForeColor = System.Drawing.Color.Crimson;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
             this.lblNome.Location = new System.Drawing.Point(-1, 9);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.Size = new System.Drawing.Size(55, 22);
             this.lblNome.TabIndex = 121;
             this.lblNome.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 183);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 145;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(424, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 22);
+            this.label1.TabIndex = 146;
+            this.label1.Text = "Número";
             // 
             // CadastroCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 261);
-            this.Controls.Add(this.button1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(533, 271);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtRua);
             this.Controls.Add(this.lblRua);
             this.Controls.Add(this.txtCid);
@@ -326,7 +339,7 @@
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.lblNome);
             this.Name = "CadastroCliente";
-            this.Text = "CadastroCliente";
+            this.Text = "Cadastro do Cliente";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -335,8 +348,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label txtEmpresa;
         private System.Windows.Forms.TextBox txtRua;
         private System.Windows.Forms.Label lblRua;
         private System.Windows.Forms.TextBox txtCid;
@@ -358,5 +369,7 @@
         private System.Windows.Forms.MaskedTextBox mskTel;
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
